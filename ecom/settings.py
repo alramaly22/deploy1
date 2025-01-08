53388da771ac859c166cf412229d083dc905832f
m.alramaly <96423919+alramaly22@users.noreply.github.com>
2025-01-08 07:06:40 +0200

    Update settings.py

--- ecom/settings.py
+++ ecom/settings.py
@@ -149,8 +149,8 @@ STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]  # هذا هو المكا�
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
-MEDIA_ROOT = os.path.join(os.environ.get('RAILWAY_VOLUME_MOUNT_PATH', BASE_DIR), 'media')
 MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
