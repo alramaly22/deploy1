1facd112c38885f0302772d364541ef9393f316f
m.alramaly <96423919+alramaly22@users.noreply.github.com>
2025-01-08 06:38:53 +0200

    Update settings.py

--- ecom/settings.py
+++ ecom/settings.py
@@ -23,10 +23,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-!(z@15mx4g9^%+c+6va$bl0c9r)-)7x0lyrg#3b#c@4t9u7km6'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 
-ALLOWED_HOSTS = ['deploy1-production-84f3.up.railway.app' ]
+ALLOWED_HOSTS = ['deploy1-production-84f3.up.railway.app', 'localhost', '127.0.0.1']
 
 
 # Application definition
